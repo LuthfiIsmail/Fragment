81e1e7ccb9fa0edcee082d1e4531dbd22c5c6546
LuthfiIsmail <61674783+LuthfiIsmail@users.noreply.github.com>
2021-01-21 07:51:46 +0700

    Add files via upload

---
+++ java/com/example/fragmentbuttonnavi/ProfileFragments.java
@@ -0,0 +1,25 @@
+package com.example.fragmentbuttonnavi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class ProfileFragments extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+}
